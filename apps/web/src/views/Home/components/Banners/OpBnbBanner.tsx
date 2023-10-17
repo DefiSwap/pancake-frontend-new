c779c50b96ef7bf1dec2d39251cadde4e60ce007
loop <edwinvarenoyogapradana@gmail.com>
2023-10-18 01:57:36 +0700

    update IDO banner

--- apps/web/src/views/Home/components/Banners/OpBnbBanner.tsx
+++ apps/web/src/views/Home/components/Banners/OpBnbBanner.tsx
@@ -8,6 +8,7 @@ import * as S from './Styled'
 import { opbnbBunny, opbnbBg, opbnbBgMobile } from './images'
 
 const pancakeSwapLogo = `${ASSET_CDN}/web/banners/ethXpancakeswap.png`
+const pattiebar = "https://i.imgur.com/FQszbms.png";
 
 const OpBnbLogo = (props: SvgProps) => (
   <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33 33" fill="none" {...props}>
@@ -145,23 +146,23 @@ export const OpBnbBanner = memo(() => {
           </Link>
         </S.LeftWrapper>
         <RightWrapper>
-          {isDesktop ? (
-            <>
-              <Image src="https://i.imgur.com/FQszbms.png" alt="opbnbBunny" width={172} height={200} placeholder="blur" />
-            </>
-          ) : (
-            <Image src="https://i.imgur.com/FQszbms.png" alt="opbnbBunny" width={151} height={176} placeholder="blur" />
-          )}
-          <BgWrapper>
-            {isDesktop ? (
-              <>
-                <Image src={opbnbBg} alt="opbnbBg" width={1128} height={192} placeholder="blur" />
-              </>
-            ) : (
-              <Image src={opbnbBgMobile} alt="opbnbBgMobile" width={358} height={181} placeholder="blur" />
-            )}
-          </BgWrapper>
-        </RightWrapper>
+  {isDesktop ? (
+    <>
+      <Image src={pattiebar}  alt="opbnbBunny" width={172} height={200} placeholder="blur" />
+    </>
+  ) : (
+    <Image src={pattiebar} alt="opbnbBunny" width={151} height={176} placeholder="blur" />
+  )}
+  <BgWrapper>
+    {isDesktop ? (
+      <>
+        <Image src={opbnbBg} alt="opbnbBg" width={1128} height={192} placeholder="blur" />
+      </>
+    ) : (
+      <Image src={opbnbBgMobile} alt="opbnbBgMobile" width={358} height={181} placeholder="blur" />
+    )}
+  </BgWrapper>
+</RightWrapper>
       </S.Inner>
     </S.Wrapper>
   )

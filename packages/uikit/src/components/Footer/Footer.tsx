79e6ac3f97936e8996fe924f78e0bc89bab44e28
loop <edwinvarenoyogapradana@gmail.com>
2023-10-18 08:52:32 +0700

    a

--- packages/uikit/src/components/Footer/Footer.tsx
+++ packages/uikit/src/components/Footer/Footer.tsx
@@ -46,7 +46,7 @@ const MenuItem: React.FC<React.PropsWithChildren<FooterProps>> = ({
     >
       <Flex flexDirection="column" width={["100%", null, "1200px;"]}>
         <StyledIconMobileContainer display={["block", null, "none"]}>
-         
+          <LogoWithTextIcon width="130px" />
         </StyledIconMobileContainer>
         <Flex
           order={[2, null, 1]}
@@ -78,7 +78,7 @@ const MenuItem: React.FC<React.PropsWithChildren<FooterProps>> = ({
               ))}
             </StyledList>
           ))}
-          <Box display={["none", null, "block"]}>{isXl ? <LogoIcon /> : <LogoWithTextIcon width="160px" />}</Box>
+          
         </Flex>
         <StyledSocialLinks order={[2]} pb={["42px", null, "32px"]} mb={["0", null, "32px"]} />
         <StyledToolsContainer

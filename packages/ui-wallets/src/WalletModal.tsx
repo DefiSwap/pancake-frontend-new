b3f142fb8ad8ce4ce6767a06459e58f76d01325f
loop <edwinvarenoyogapradana@gmail.com>
2023-10-18 22:23:16 +0700

    wallet intro logo

--- packages/ui-wallets/src/WalletModal.tsx
+++ packages/ui-wallets/src/WalletModal.tsx
@@ -401,7 +401,7 @@ export function WalletModalV2<T = unknown>(props: WalletModalV2Props<T>) {
       wallets
         .map((w) => w.icon)
         .filter((icon) => typeof icon === 'string')
-        .concat('https://cdn.pancakeswap.com/wallets/wallet_intro.png') as string[],
+        .concat('https://i.ibb.co/qgCjtjj/logo.png') as string[],
     [wallets],
   )
 
@@ -458,7 +458,7 @@ const Intro = ({ docLink, docText }: { docLink: string; docText: string }) => {
       <Heading as="h1" fontSize="20px" color="secondary">
         {t('Haven’t got a wallet yet?')}
       </Heading>
-      <Image src="https://cdn.pancakeswap.com/wallets/wallet_intro.png" width={198} height={178} />
+      <Image src="https://i.ibb.co/qgCjtjj/logo.png" width={198} height={178} />
       <Button as={LinkExternal} color="backgroundAlt" variant="subtle" href={docLink}>
         {docText}
       </Button>

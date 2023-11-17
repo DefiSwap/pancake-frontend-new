84054c942db452c59bd7bb906b093e10a7126d8a
loopingdev <81473630+judithjr@users.noreply.github.com>
2023-11-18 04:30:26 +0700

    Revert "remove chest opbnbbanner and booster image"
    This reverts commit c0b3590632b7f57c73596255c33ac61b45ff7bfd.

--- apps/web/src/views/Farms/components/YieldBooster/components/bCakeV3/BCakeBoosterCard.tsx
+++ apps/web/src/views/Farms/components/YieldBooster/components/bCakeV3/BCakeBoosterCard.tsx
@@ -115,7 +115,9 @@ export const BCakeBoosterCard = () => {
   })
   return (
     <CardWrapper>
-    
+      <ImageWrapper>
+        <Image src={boosterCardImage} alt="boosterCardImage" width={99} height={191} placeholder="blur" />
+      </ImageWrapper>
       <Card p="0px" style={{ zIndex: 1 }}>
         <StyledCardBody style={{ padding: '15px 24px' }}>
           <RocketIcon />

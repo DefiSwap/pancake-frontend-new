f16d493fc5875eafd8d34c8abc276122ead7721c
loopingdev <81473630+judithjr@users.noreply.github.com>
2023-11-18 06:20:05 +0700

    update meta and theme to dark

--- apps/aptos/components/Menu/Menu.tsx
+++ apps/aptos/components/Menu/Menu.tsx
@@ -53,7 +53,7 @@ export const Menu = ({ children }: { children: ReactNode }) => {
   const isDark = resolvedTheme === 'dark'
 
   const toggleTheme = useMemo(() => {
-    return () => setTheme(isDark ? 'light' : 'dark')
+    return () => setTheme(isDark ? 'dark' : 'dark')
   }, [setTheme, isDark])
 
   const getFooterLinks = useMemo(() => {

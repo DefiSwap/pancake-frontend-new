af01af8873fd9df6d00af54d34cee00dac4b83d7
loopingdev <81473630+judithjr@users.noreply.github.com>
2023-11-18 07:47:37 +0700

    remove network

--- apps/web/src/components/NetworkSwitcher.tsx
+++ apps/web/src/components/NetworkSwitcher.tsx
@@ -123,23 +123,8 @@ const WrongNetworkSelect = ({ switchNetwork, chainId }) => {
 }
 
 const SHORT_SYMBOL = {
-  [ChainId.ETHEREUM]: 'ETH',
   [ChainId.BSC]: 'BNB',
-  [ChainId.BSC_TESTNET]: 'tBNB',
-  [ChainId.GOERLI]: 'GOR',
-  [ChainId.ARBITRUM_ONE]: 'ARB',
-  [ChainId.ARBITRUM_GOERLI]: 'tARB',
-  [ChainId.POLYGON_ZKEVM]: 'Polygon zkEVM',
-  [ChainId.POLYGON_ZKEVM_TESTNET]: 'tZkEVM',
-  [ChainId.ZKSYNC]: 'zkSync',
-  [ChainId.ZKSYNC_TESTNET]: 'tZkSync',
-  [ChainId.LINEA]: 'Linea',
-  [ChainId.LINEA_TESTNET]: 'tLinea',
-  [ChainId.OPBNB]: 'opBNB',
-  [ChainId.OPBNB_TESTNET]: 'tOpBNB',
-  [ChainId.BASE]: 'Base',
-  [ChainId.BASE_TESTNET]: 'tBase',
-  [ChainId.SCROLL_SEPOLIA]: 'tScroll',
+ 
 } as const satisfies Record<ChainId, string>
 
 export const NetworkSwitcher = () => {

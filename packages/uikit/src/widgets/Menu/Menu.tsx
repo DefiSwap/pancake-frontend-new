1c40c8a289cdbf6c016a88b389209a1b1983c4f5
loopingdev <81473630+judithjr@users.noreply.github.com>
2023-11-18 16:09:52 +0700

    Update Menu.tsx

--- packages/uikit/src/widgets/Menu/Menu.tsx
+++ packages/uikit/src/widgets/Menu/Menu.tsx
@@ -205,11 +205,12 @@ const Menu: React.FC<React.PropsWithChildren<NavProps>> = ({
         setLang={setLang}
         currentLang={currentLang}
         cakePriceUsd={cakePriceUsd}
-        
+        buyCakeLabel={buyCakeLabel}
+        buyCakeLink={buyCakeLink}
         mb={[`${MOBILE_MENU_HEIGHT}px`, null, "0px"]}
       />
       <AtomBox display={{ xs: "block", md: "none" }}>
-        <BottomNav items={links} activeItem={activeItem} activeSubItem={activeSubItem} />
+        
       </AtomBox>
     </MenuContext.Provider>
   );

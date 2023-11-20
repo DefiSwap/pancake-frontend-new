9b6535fa89d8be64ef7657e2c0e17e2f0b4b47ee
loopingdev <81473630+judithjr@users.noreply.github.com>
2023-11-21 00:59:09 +0700

    as

--- apps/web/src/views/V3Info/components/Loader/index.tsx
+++ apps/web/src/views/V3Info/components/Loader/index.tsx
@@ -78,7 +78,7 @@ export const LocalLoader = ({ fill }: { fill: boolean }) => {
   return (
     <Wrapper fill={fill ? 1 : 0}>
       <AnimatedImg>
-        <img src="https://assets.pancakeswap.finance/web/pancake-3d-spinner-v2.gif" alt="loading-icon" />
+        <img src="https://raw.githubusercontent.com/Pattieswap/assets/main/logo.png" alt="loading-icon" />
       </AnimatedImg>
     </Wrapper>
   )

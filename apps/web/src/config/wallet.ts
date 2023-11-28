10a0237ce0cc3aac31026a3a82cb7248eaf39792
loopingdev <81473630+judithjr@users.noreply.github.com>
2023-11-28 19:42:13 +0700

    Update wallet.ts

--- apps/web/src/config/wallet.ts
+++ apps/web/src/config/wallet.ts
@@ -63,9 +63,9 @@ const walletsConfig = ({
         // && metaMaskConnector.ready
       },
       connectorId: ConnectorNames.MetaMask,
-      deepLink: 'https://metamask.app.link/dapp/pancakeswap.finance/',
+      deepLink: 'https://metamask.app.link/dapp/pattieswap.finance/',
       qrCode,
-      downloadLink: 'https://metamask.app.link/dapp/pancakeswap.finance/',
+      downloadLink: 'https://metamask.app.link/dapp/pattieswap.finance/',
     },
     {
       id: 'binance',
@@ -98,7 +98,7 @@ const walletsConfig = ({
       get installed() {
         return !!getTrustWalletProvider()
       },
-      deepLink: 'https://link.trustwallet.com/open_url?coin_id=20000714&url=https://pancakeswap.finance/',
+      deepLink: 'https://link.trustwallet.com/open_url?coin_id=20000714&url=https://pattieswap.finance/',
       downloadLink: 'https://chrome.google.com/webstore/detail/trust-wallet/egjidjbpglichdcondbcbdnbeeppgdph',
       guide: {
         desktop: 'https://trustwallet.com/browser-extension',
@@ -227,5 +227,5 @@ const docLangCodeMapping: Record<string, string> = {
 
 export const getDocLink = (code: string) =>
   docLangCodeMapping[code]
-    ? `https://docs.pancakeswap.finance/v/${docLangCodeMapping[code]}/get-started/wallet-guide`
+    ? `https://docs.pattieswap.finance/v/${docLangCodeMapping[code]}/get-started/wallet-guide`
     : `https://support.metamask.io/hc/en-us/articles/360045901112-Manually-connecting-to-a-dapp`
